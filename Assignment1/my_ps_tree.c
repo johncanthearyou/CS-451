ee13c3c2aa2e3dc22e0c2931e3ecaac20deebd26
John Stockton <johnstocktoniv@gmail.com>
2022-07-23 12:43:19 -0400

    Guard bad pid

--- Assignment1/my_ps_tree.c
+++ Assignment1/my_ps_tree.c
@@ -2,3 +2,25 @@
 // CS-451
 // Dr. Kanchi
 // Summer 2022
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int mypstree(int pid) {
+    // Guard non-existant process
+    int pidExists = getpgid(pid) >= 0;
+    if (!pidExists) {
+        printf("ERROR! PID: %d does not exist. Program will exit.\n", pid);
+        exit(3);
+    }
+
+
+    return -1;
+}
+
+int main() {
+    printf("%d", mypstree(69));
+}
